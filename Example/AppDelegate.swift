0481338c06d86e846785751eb5a8a28246a26183
Kristian Angyal <kristian.angyal@mailonline.co.uk>
2016-03-22 16:24:57 +0000

    Needs to go too.

--- Example/AppDelegate.swift
+++ Example/AppDelegate.swift
@@ -14,8 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        
-        //application.windows.first?.layer.speed = 0.1
+
         return true
     }
 }
