4b5c1cca9053a7e89c415874d88aeaa937b018b9
Rui Peres <rui.peres@mailonline.co.uk>
2016-03-30 13:21:06 +0100

    Small fix

--- ImageViewer/Source/ImageFadeInHandler.swift
+++ ImageViewer/Source/ImageFadeInHandler.swift
@@ -10,7 +10,7 @@ import Foundation
 
 final class ImageFadeInHandler {
     
-    private var presentedImages:[Int] = []
+    private var presentedImages: [Int] = []
     
     func addPresentedImageIndex(index: Int) {
         
