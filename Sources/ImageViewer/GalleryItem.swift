e97c1e84b3346301dd13e036f707c55badfe98c6
Bruno Guidolim <bguidolim@gmail.com>
2020-03-24 13:27:56 +0100

    Added @escaping notation for custom gallery item

--- Sources/ImageViewer/GalleryItem.swift
+++ Sources/ImageViewer/GalleryItem.swift
@@ -10,7 +10,7 @@ import UIKit
 
 public typealias ImageCompletion = (UIImage?) -> Void
 public typealias FetchImageBlock = (@escaping ImageCompletion) -> Void
-public typealias ItemViewControllerBlock = (_ index: Int, _ itemCount: Int, _ fetchImageBlock: FetchImageBlock, _ configuration: GalleryConfiguration, _ isInitialController: Bool) -> UIViewController
+public typealias ItemViewControllerBlock = (_ index: Int, _ itemCount: Int, _ fetchImageBlock: @escaping FetchImageBlock, _ configuration: GalleryConfiguration, _ isInitialController: Bool) -> UIViewController
 
 public enum GalleryItem {
 
